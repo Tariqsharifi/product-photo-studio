98232d967bd3115d408d83016189dacc177227e4
Tariqsharifi <tariqsharifi534@gmail.com>
2026-08-26 11:44:00 +0330

    Create schema.ts

---
+++ convex/schema.ts
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    email: v.string(),
+    name: v.optional(v.string()),
+    token: v.optional(v.string()),
+    code: v.optional(v.string()),
+    codeExpiry: v.optional(v.number()),
+  }).index("by_email", ["email"])
+    .index("by_token", ["token"]),
+});
